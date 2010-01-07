243433d85162498533fd156380a40d4195177865
Philipp von Weitershausen <philipp@weitershausen.de>
2010-01-07 16:36:02 +0000

    My first jetpack: display the ubiquity window whenever a new blank tab is opened

---
+++ newtabubiquity.js
@@ -0,0 +1,13 @@
+var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+         .getService(Components.interfaces.nsIWindowMediator);
+var tabbrowser = wm.getMostRecentWindow("navigator:browser").getBrowser();
+
+
+/* When opening a new blank tab, open the Ubiquity window */
+function onDelayedTabOpen() {
+  if (tabbrowser.currentURI.spec == "about:blank") {
+    wm.getMostRecentWindow("navigator:browser").window.gUbiquity.openWindow();
+  }
+}
+tabbrowser.addEventListener('TabOpen',
+  function(event) { setTimeout(onDelayedTabOpen, 0); }, true);
